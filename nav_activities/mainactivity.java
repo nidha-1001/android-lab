1140899da3cfa5528268c858458fba5ead985155
nidha-1001 <nidhaknidhak321@gmail.com>
2026-09-14 15:15:28 +0530

    done

---
+++ nav_activities/mainactivity.java
@@ -0,0 +1,50 @@
+package com.example.optionmenuapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_home) {
+
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+
+        if (item.getItemId() == R.id.menu_about) {
+
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
